4805a19592f9ec6e1d67e1dbf6c4f2fb681bbac7
anilgehllot <anilgehlot.edu2k4@gmail.com>
2025-02-27 10:30:39 +0530

    completed-ca

---
+++ my-app/Team/team-showcase./src/components/TeamMemberCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const TeamMemberCard = ({name,title,experience}) => {
+  return (
+    <div>
+      <h1>{name}</h1>  <h3>{title}</h3>
+      <br>
+      </br>
+      <br>
+      </br>
+    </div>
+  )
+}
+
+export default TeamMemberCard
